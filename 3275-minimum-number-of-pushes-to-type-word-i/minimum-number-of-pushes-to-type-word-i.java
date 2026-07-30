1cd2ca52e0e791b7699afad0e6306cfc8106fbb3
Unnati gupta <unnatig521@gmail.com>
2026-07-30 20:25:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (28.23%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int n = word.length();
+        int ans = 0;
+        int press = 1;
+
+        for(int i = 0; i < n; i++) {
+            if(i > 0 && i % 8 == 0) {
+                press++;
+            }
+            ans += press;
+        }
+
+        return ans;
+    }
+}
