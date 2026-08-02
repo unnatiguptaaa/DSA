d307bfdb0ccc32e71361a10f122ad271640f648c
Unnati gupta <unnatig521@gmail.com>
2026-08-02 18:36:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (95.35%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -2,26 +2,28 @@ class Solution {
 
     public boolean stoneGame(int[] piles) {
 
-        int n = piles.length;
-        Integer[][] dp = new Integer[n][n];
+    //    int n = piles.length;
+    //     Integer[][] dp = new Integer[n][n];
 
-        return solve(piles, 0, n - 1, dp) > 0;
-    }
+    //     return solve(piles, 0, n - 1, dp) > 0;
+    // }
 
-    public int solve(int[] piles, int i, int j, Integer[][] dp) {
+    // public int solve(int[] piles, int i, int j, Integer[][] dp) {
 
-        if (i == j)
-            return piles[i];
+    //     if (i == j)
+    //         return piles[i];
 
-        if (dp[i][j] != null)
-            return dp[i][j];
+    //     if (dp[i][j] != null)
+    //         return dp[i][j];
 
-        int takeLeft = piles[i] - solve(piles, i + 1, j, dp);
+    //     int takeLeft = piles[i] - solve(piles, i + 1, j, dp);
 
-        int takeRight = piles[j] - solve(piles, i, j - 1, dp);
+    //     int takeRight = piles[j] - solve(piles, i, j - 1, dp);
 
-        dp[i][j] = Math.max(takeLeft, takeRight);
+    //     dp[i][j] = Math.max(takeLeft, takeRight);
 
-        return dp[i][j];
+    //     return dp[i][j];
+
+    return true;
     }
 }
