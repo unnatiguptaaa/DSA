6f27a7fc171e40da6ae2098f08eaab6acbd7d0e7
Unnati gupta <unnatig521@gmail.com>
2026-09-02 12:09:01 +0530

    Time: 1 ms (31.89%) | Memory: 45.3 MB (35.83%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+      
+        int odd = 0;
+        int even = 0;
+
+        for (int num : nums1) {
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        // All elements already have same parity
+        if (odd == 0 || even == 0) {
+            return true;
+        }
+
+        // Both odd and even exist:
+        // Every element can be made odd by subtracting
+        // an element of opposite parity.
+        return true;
+    }
+}
+        
+    
