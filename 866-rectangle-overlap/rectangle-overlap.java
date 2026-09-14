b58de0e1e17f533e33ef501d5e0a546b1543185e
Unnati gupta <unnatig521@gmail.com>
2026-09-14 09:46:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (17.64%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+
+        if (rec1[2] <= rec2[0] ||   // rec1 is left of rec2
+            rec2[2] <= rec1[0] ||   // rec2 is left of rec1
+            rec1[3] <= rec2[1] ||   // rec1 is below rec2
+            rec2[3] <= rec1[1]) {   // rec2 is below rec1
+
+            return false;
+        }
+
+        return true;
+    }
+}
